397594eb3e9f3747fbc23c2bdc29d5a794ff202a
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-08 22:40:19 +0530

    feat: add Drawer and SideBarMenu components, integrate with NavBar and LogInButton

---
+++ src/components/ui/sidebar-menu.tsx
@@ -0,0 +1,68 @@
+import { useState, ReactNode } from "react"
+import { Drawer, DrawerContent } from "@/components/ui/drawer"
+import { X, Menu } from 'lucide-react'
+import { motion } from "framer-motion"
+import { Link } from "react-scroll"
+import { cn } from "@/lib/utils"
+import LogInButton from "./logIn-button"
+import { ThemeToggle } from "../theme-toogle"
+import { NavBar } from "./tubelight-navbar"
+
+interface NavItem {
+  name: string
+  url: string
+  icon: ReactNode
+}
+
+interface NavBarProps {
+  items: NavItem[]
+  className?: string
+}
+
+function SideBarMenu({ items, className }: NavBarProps) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <div
+      className={cn(
+        "",
+        className,
+      )}
+    >
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-2">
+            <button role='button' className='p-2 bg-border rounded-full' 
+              onClick={() => {
+                setOpen(true)
+              }}
+            >
+                <Menu className='text-secondary hover:scale-105'/>
+            </button>
+        </div>
+
+        <Drawer open={open} onOpenChange={setOpen}>
+          <DrawerContent side={'right'}>
+          <div className="flex flex-col items-start ml-8 gap-5 bg-background/5 mt-5">
+
+          <div className="flex gap-2 items-center self-end mr-4">
+            <ThemeToggle />
+            <button role='button' className='p-2 bg-border rounded-full' 
+                onClick={() => {
+                  setOpen(false)
+                }}
+              >
+                  <X className='text-secondary hover:scale-105'/>
+            </button>
+          </div>
+
+          <NavBar items={items} open={open} />
+          <LogInButton className="self-end mr-4 mt-10" />
+        </div>
+          </DrawerContent>
+        </Drawer>
+      </div>
+    </div>
+  )
+}
+
+export default SideBarMenu
--- src/components/ui/tubelight-navbar.tsx
+++ src/components/ui/tubelight-navbar.tsx
@@ -1,7 +1,6 @@
-import React, { ReactElement, ReactNode, useEffect, useState } from "react"
+import { ReactNode, useState } from "react"
 import { motion } from "framer-motion"
 import { Link } from "react-scroll"
-import { LucideIcon } from "lucide-react"
 import { cn } from "@/lib/utils"
 
 interface NavItem {
@@ -13,30 +12,23 @@ interface NavItem {
 interface NavBarProps {
   items: NavItem[]
   className?: string
+  open?: boolean
 }
 
-export function NavBar({ items, className }: NavBarProps) {
+export function NavBar({ items, className, open }: NavBarProps) {
   const [activeTab, setActiveTab] = useState(items[0].name)
-  const [isMobile, setIsMobile] = useState(false)
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768)
-    }
-
-    handleResize()
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  }, [])
 
   return (
     <div
       className={cn(
-        "fixed left-1/2 -translate-x-1/2 z-50",
+        "",
         className,
       )}
     >
-      <div className="flex items-center gap-3 bg-background/5 border border-border backdrop-blur-lg py-1 px-1 rounded-full shadow-lg">
+      <div className={cn(
+        !open && "flex items-center gap-3 bg-background/5 border border-border backdrop-blur-lg py-1 px-1 rounded-full shadow-lg",
+        open && "flex flex-col items-start gap-5 bg-background/5 mt-5"
+      )}>
         {items.map((item) => {
           const isActive = activeTab === item.name
 
@@ -47,20 +39,25 @@ export function NavBar({ items, className }: NavBarProps) {
               onClick={() => setActiveTab(item.name)}
               className={cn(
                 "relative cursor-pointer text-sm font-semibold px-6 py-2 rounded-full transition-colors",
-                "text-foreground/80 hover:text-primary",
-                isActive && "bg-muted text-primary",
+                "text-secondary hover:text-primary",
+                open && "border border-border backdrop-blur-lg py-2 px-4 shadow-lg",
+                isActive && "bg-muted/20 text-secondary",
+                isActive && open && "rounded-bl-none rounded-tl-none"
               )}
             >
               <div className="flex gap-2">
-                <span className="text-[#000] dark:text-[#fff]">
+                <span className="">
                   {item.icon}
                 </span>
-                <span className="text-[#000] dark:text-[#fff]">{item.name}</span>
+                <span className="">{item.name}</span>
               </div>
               {isActive && (
                 <motion.div
                   layoutId="lamp"
-                  className="absolute inset-0 w-full bg-primary/5 rounded-full -z-10"
+                  className={cn(
+                    "absolute inset-0 w-full bg-primary/5 rounded-full -z-10",
+                    open && "rounded-bl-none rounded-tl-none"
+                  )}
                   initial={false}
                   transition={{
                     type: "spring",
@@ -68,10 +65,10 @@ export function NavBar({ items, className }: NavBarProps) {
                     damping: 30,
                   }}
                 >
-                  <div className="absolute -top-2 left-1/2 -translate-x-1/2 w-8 h-1 bg-primary rounded-t-full">
-                    <div className="absolute w-12 h-6 bg-primary/20 rounded-full blur-md -top-2 -left-2" />
-                    <div className="absolute w-8 h-6 bg-primary/20 rounded-full blur-md -top-1" />
-                    <div className="absolute w-4 h-4 bg-primary/20 rounded-full blur-sm top-0 left-2" />
+                  <div className={`absolute ${open ? '-top-[2px] left-0 w-1 h-10 rounded-full' : '-top-2 left-1/2 w-8 h-1 rounded-t-full'} -translate-x-1/2 bg-primary`}>
+                    <div className={`absolute w-12 h-6 bg-primary/20 rounded-full blur-md ${open ? 'top-4 -left-0' : '-top-2 -left-2'} `} />
+                    <div className={`absolute w-8 h-6 bg-primary/20 rounded-full blur-md  ${open ? 'top-2' : '-top-1'}`} />
+                    <div className={`absolute w-4 h-4 bg-primary/20 rounded-full blur-sm top-0  ${open ? 'left-0' : 'left-2'}`} />
                   </div>
                 </motion.div>
               )}
