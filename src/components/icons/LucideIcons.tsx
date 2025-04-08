babaa3377aaefcc454f5ee490fc87955e79dba0e
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-08 16:50:22 +0530

    feat: add LucideIcons and Udemy icon components

---
+++ src/components/icons/LucideIcons.tsx
@@ -0,0 +1,19 @@
+import { GraduationCap, ScrollText , MessageCircleHeart } from 'lucide-react'
+
+export function MyGraduationCap() {
+  return (
+    <GraduationCap size={18} strokeWidth={2.5} className='text-[#FF6B00]' />
+  )
+}
+
+export function MyScrollText() {
+  return (
+    <ScrollText size={18} strokeWidth={2.5} className='text-[#1167B1]' />
+  )
+}
+
+export function MyMessageCircleHeart() {
+  return (
+    <MessageCircleHeart size={18} strokeWidth={2.5} className='text-[#B32113]' />
+  )
+}
---
+++ src/components/icons/Udemy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Udemy() {
+  return (
+    <img src='./src/assets/udemy-icon-svgrepo-com.svg' style={{minWidth: 18}} width={18}/>
+  )
+}
+
+export default Udemy
