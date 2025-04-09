804f7f6e3adedf5dc1bca9cafdef6ed6e049fbbc
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-09 11:10:37 +0530

    feat: add hover effects to ChaicodeLogo component

--- src/components/ui/chaicode-logo.tsx
+++ src/components/ui/chaicode-logo.tsx
@@ -1,6 +1,6 @@
 function ChaicodeLogo({theme}: {theme: string}) {
   return (
-    <img src={`${theme === 'dark' ? './src/assets/chaicode-white.svg' : './src/assets/chaicode-black.svg'}`} width='130px' />
+    <img className="hover:scale-105 hover:drop-shadow-lg" src={`${theme === 'dark' ? './src/assets/chaicode-white.svg' : './src/assets/chaicode-black.svg'}`} width='130px' />
   )
 }
 
