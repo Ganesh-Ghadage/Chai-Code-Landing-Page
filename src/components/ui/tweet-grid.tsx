d5d8038ffb6ccca42eb44d0a7cfdcbcce296be7d
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-23 18:28:14 +0530

    feat: add animation effects to TweetGrid component; implement entrance animations for tweets on scroll

--- src/components/ui/tweet-grid.tsx
+++ src/components/ui/tweet-grid.tsx
@@ -2,6 +2,8 @@ import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 import { TweetCard } from "./tweet-card"
+import { motion, useAnimate, useInView, stagger } from "motion/react";
+import { useEffect } from "react";
 
 const tweetGridVariants = cva("max-w-4xl md:max-w-6xl px-2 mx-auto w-full", {
   variants: {
@@ -39,15 +41,42 @@ export interface TweetGridProps
 }
 
 function TweetGrid({ tweets, columns, spacing, className }: TweetGridProps) {
+  const [scope, animate] = useAnimate()
+  const isInView = useInView(scope)
+
+  useEffect(() => {
+    if (isInView) {
+      animate(
+        "div", 
+        {
+          opacity: 1,
+          filter: 'blur(0px)'
+        },
+        {
+          duration: 0.5,
+          delay: stagger(0.05 , {startDelay: 0.3}),
+          ease: "easeInOut"
+        }
+      )
+    }
+  }, [isInView])
+  
   return (
-    <div className={cn(tweetGridVariants({ columns }), className)}>
+    <div 
+      ref={scope} 
+      className={cn(tweetGridVariants({ columns }), className)}
+    >
       {tweets.map((tweetId, i) => (
-        <div
+        <motion.div
           key={`${tweetId}-${i}`}
           className={cn(tweetItemVariants({ spacing }), "overflow-hidden mb-4 text-secondary")}
+          style={{
+            opacity: 0,
+            filter: 'blur(5px)'
+          }}
         >
           <TweetCard id={tweetId} />
-        </div>
+        </motion.div>
       ))}
     </div>
   )
