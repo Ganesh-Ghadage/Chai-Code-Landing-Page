b3e2fe23a6a8389b618cdb915fa31c37c2708ad8
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-23 16:35:04 +0530

    feat: replace static BottomLine component with animated motion.div; remove unused slide animation from CSS

--- src/components/bottom-line.tsx
+++ src/components/bottom-line.tsx
@@ -1,15 +1,30 @@
 import { cn } from "@/lib/utils"
+import { motion } from "motion/react"
 
 function BottomLine({className} : {className?: string}) {
   return (
-    <div className={cn(
-      'flex justify-center bg-primary/30 w-[70%] md:w-[110%] h-1 rounded-full transition-all slide',
-      className
-    )}>
+    <motion.div
+      initial={{
+        width: 0,
+        opacity: 0,
+      }} 
+      whileInView={{
+        width: '100%',
+        opacity: 1,
+        transition: {
+          duration: 0.3,
+          ease: "easeInOut"
+        }
+      }}
+      className={cn(
+        'flex justify-center bg-primary/30 w-[70%] md:w-[110%] h-1 rounded-full',
+        className
+      )}
+    >
       <div className='flex justify-center bg-primary/60 w-[70%] h-1 rounded-full '>
         <div className='bg-primary w-[30%] h-1 rounded-full '></div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
