0e58260f4a2bf3db2bfe764093517b76aba21842
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-25 16:37:03 +0530

    feat: enhance Community section with AnimateText and motion effects for improved user engagement

--- src/components/sections/Community.tsx
+++ src/components/sections/Community.tsx
@@ -1,6 +1,7 @@
+import AnimateText from "../animate-text"
 import Heading from "../heading"
 import { Button } from "../ui/button"
-
+import { motion } from "motion/react"
 
 function Community() {
   return (
@@ -8,30 +9,87 @@ function Community() {
       <Heading heading="Community" />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <div className="flex flex-col gap-10 justify-center">
-          <h1 className="text-secondary text-4xl md:text-6xl text-wrap text-center md:text-start font-bold">Join our community where creativity thrives.</h1>
-          <Button className="w-fit p-4 self-center md:self-start">80,000 Active coders in Discord</Button>
-          <Button 
-            className="w-fit self-center bg-transparent border text-[#5865F2] py-8 px-4 hover:bg-[#E0E3FF]"
+          <AnimateText text={"Join our community where creativity thrives."} className={"text-secondary text-4xl md:text-6xl text-wrap text-center md:text-start font-bold mx-auto"} />
+          <motion.div  
+            initial={{
+              x: 20,
+              opacity: 0
+            }}
+            whileInView={{
+              x: 0,
+              opacity: 1,
+              transition: {
+                delay: 0.3,
+                type: "spring",
+              }
+            }}
+            viewport={{ once: true }}
+            key={`active-btn`}
+            className="w-fit p-4 self-center md:self-start"
           >
-            <a href="https://hitesh.ai/discord"
-              className="w-full flex gap-4"
+            <Button>80,000 Active coders in Discord</Button>
+          </motion.div>
+          <motion.div 
+            initial={{
+              x: -20,
+              opacity: 0
+            }}
+            whileInView={{
+              x: 0,
+              opacity: 1,
+              transition: {
+                delay: 0.4,
+                type: "spring",
+              }
+            }}
+            viewport={{ once: true }}
+            key={`join-btn`}
+            className="w-fit self-center bg-transparent cursor-pointer"
+          >
+            <Button 
+              className="w-fit self-center bg-transparent border text-[#5865F2] py-8 px-4 hover:bg-[#E0E3FF]"
             >
-              <img 
-                src="./src/assets/Discord-Symbol-Blurple.png"  
-                alt="discord logo"
-                className="w-10 object-contain cursor-pointer"  
-              />
-              <span className="text-lg font-bold">Join Discord</span>
-            </a>
-          </Button>
+              <a href="https://hitesh.ai/discord"
+                className="w-full flex gap-4"
+              >
+                <img 
+                  src="./src/assets/Discord-Symbol-Blurple.png"  
+                  alt="discord logo"
+                  className="w-10 object-contain cursor-pointer"  
+                />
+                <span className="text-lg font-bold">Join Discord</span>
+              </a>
+            </Button>
+          </motion.div>
         </div>
-        <div className="max-w-xl min-h-[25rem] mx-auto overflow-hidden rounded-4xl">
-          <img 
+        <motion.div 
+          className="max-w-xl min-h-[25rem] mx-auto overflow-hidden rounded-4xl"
+          initial={{
+            scale: 0.8,
+            opacity: 0
+          }}
+          whileInView={{
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: 1,
+              type: "spring",
+            }
+          }}
+          viewport={{ once: true }}
+        >
+          <motion.img 
+            whileHover={{
+              scale: 1.1
+            }}
+            whileTap={{
+              scale: [1.1, 1, 1.1]
+            }}
             src="./src/assets/Discord-Banner_8.png" 
             alt="DiscordBanner" 
-            className="object-cover w-full h-full rounded-4xl hover:scale-110 ease-in-out"
+            className="object-cover w-full h-full rounded-4xl"
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   )
