0f67aabe835ccec0838bb7e4e2d6a587b3d661f5
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-25 17:47:11 +0530

    feat: replace LogIn button with motion-enabled button for enhanced user interaction

--- src/components/logIn-button.tsx
+++ src/components/logIn-button.tsx
@@ -1,13 +1,21 @@
 import { cn } from "@/lib/utils"
+import { motion } from "motion/react"
 
 function LogInButton({className} : {className?: string}) {
   return (
-    <button className={cn(
-      'px-4 py-2 bg-[#1167B1] hover:bg-[#1166b1cf] rounded-[20px] text-white border-none',
-      className
-    )}>
-      Log In
-    </button>
+    <motion.button 
+      className={cn(
+        'px-4 py-1.5 bg-primary hover:bg-primary/20 rounded-[20px] border-2 border-primary text-white text-center',
+          className
+      )}
+      whileTap={{
+        scale: [0.9, 1, 0.9]
+      }}
+    >
+      <a href="https://courses.chaicode.com/learn/account/signin">
+        Login
+      </a>
+    </motion.button>
   )
 }
 
